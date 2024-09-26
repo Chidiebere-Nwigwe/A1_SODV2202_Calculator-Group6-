192340d498f2d0c558bb7cab037a14016030c6fd
Chidiebere Nwigwe <chidonnwigz@gmail.com>
2024-09-26 16:10:21 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1_SODV2202_Calculator
+{
+
+        // TODO Add supporting classes here
+    public class Program
+
+    {
+
+        public static string ProcessCommand(string input)
+
+        {
+
+            try
+
+            {
+
+
+
+
+
+
+                return "";
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                return "Error evaluating expression: " + e;
+
+            }
+
+        }
+
+        static void Main(string[] args)
+
+        {
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "exit")
+
+            {
+
+                Console.WriteLine(ProcessCommand(input));
+
+            }
+
+        }
+
+    }
+
+}
+
